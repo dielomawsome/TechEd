62f97e0b6143a01d5791a59f129a7c5a4220d447
shaunb-beta <shaun.bradridge@betadigital.com.au>
2023-10-30 17:01:59 +1100

    Update aws-service.js
    Filtered out non-PDF results

--- CAP/caprisks/srv/aws-service.js
+++ CAP/caprisks/srv/aws-service.js
@@ -4,7 +4,7 @@ var parser = require('xml2json');
 
 module.exports = cds.service.impl(async (srv) => {
     srv.on('FileList', async (request) => {
-
+    
         try {
             const response = await fetch("https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/");
             if (!response.ok) {
@@ -16,10 +16,14 @@ module.exports = cds.service.impl(async (srv) => {
             const filesJson = JSON.parse(data);
             
             if (filesJson && filesJson.ListBucketResult && filesJson.ListBucketResult.Contents) {
-                
+            
                 const files = filesJson.ListBucketResult.Contents.map(file=>{
                     file.FileLocation = "https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/" + file.Key
                     return file
+                }).filter(file=>{
+                    if(file.Key.endsWith(".pdf")){
+                        return file
+                    }
                 })
                 return files;
             } else {
