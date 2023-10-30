15c3a5fec3bb42d8899de1a23d2fe1701fef9d83
shaunb-beta <shaun.bradridge@betadigital.com.au>
2023-10-30 18:37:25 +1100

    Change to FileLocation - full path to PDF now available

--- CAP/caprisks/srv/aws-service.cds
+++ CAP/caprisks/srv/aws-service.cds
@@ -1,10 +1,7 @@
 @protocol: 'rest'
-
 @open
 service AWSService {
-     type object {};
-
-      function FileList() returns array of object;
-      function File(FileName: String) returns object;
-      action UploadFile() returns object;
+    type object {}; // Allows us to work with unstructered data
+    function FileList()              returns array of object; // returns a list of files uing the AWS S3 bucket objects
+    function File(FileName : String) returns object; // returns a single file in Base64
 }
--- CAP/caprisks/srv/aws-service.js
+++ CAP/caprisks/srv/aws-service.js
@@ -1,91 +1,94 @@
-var parser = require('xml2json');
-
-
+var parser = require("xml2json");
+var { URLSearchParams } = require("url");
 
 module.exports = cds.service.impl(async (srv) => {
-    srv.on('FileList', async (request) => {
-    
-        try {
-            const response = await fetch("https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/");
-            if (!response.ok) {
-                throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
-            }
-            
-            const result = await response.text();
-            const data = parser.toJson(result);
-            const filesJson = JSON.parse(data);
-            
-            if (filesJson && filesJson.ListBucketResult && filesJson.ListBucketResult.Contents) {
-            
-                const files = filesJson.ListBucketResult.Contents.map(file=>{
-                    file.FileLocation = "/rest/aws/File(" + file.Key + ")"
-                    return file
-                }).filter(file=>{
-                    if(file.Key.endsWith(".pdf")){
-                        return file
-                    }
-                })
-                return files;
-            } else {
-                throw new Error('Invalid response data structure');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            throw error; // Re-throw the error for better error handling up the chain
-        }
-    });
-    
+  srv.on("FileList", async (request) => {
+    try {
+      const response = await fetch(
+        "https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/"
+      );
+      if (!response.ok) {
+        throw new Error(
+          `HTTP Error: ${response.status} - ${response.statusText}`
+        );
+      }
 
+      const result = await response.text();
+      const data = parser.toJson(result);
+      const filesJson = JSON.parse(data);
 
-srv.on('File', async (req) => {
+      if (
+        filesJson &&
+        filesJson.ListBucketResult &&
+        filesJson.ListBucketResult.Contents
+      ) {
+        const files = filesJson.ListBucketResult.Contents.map((file) => {
+          file.FileLocation = "https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/" + file.Key;
+          return file;
+        }).filter((file) => {
+          if (file.Key.endsWith(".pdf")) {
+            return file;
+          }
+        });
+        return files;
+      } else {
+        throw new Error("Invalid response data structure");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      throw error; // Re-throw the error for better error handling up the chain
+    }
+  });
+
+  srv.on("File", async (req) => {
     if (!req.data.FileName) {
-        console.log(req.data)
-        throw new Error('Missing FileName in request data');
+      console.log(req.data);
+      throw new Error("Missing FileName in request data");
     }
 
     try {
-        const url = `https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/${req.data.FileName}`;
+      const url = `https://2jm9jcmsc5.execute-api.us-east-1.amazonaws.com/v1/appgyver-1/${req.data.FileName}`;
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/pdf");
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/pdf");
 
-        var requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      const response = await fetch(url, requestOptions);
+
+      if (!response.ok) {
+        throw new Error(
+          `HTTP Error: ${response.status} - ${response.statusText}`
+        );
+      }
+
+      // Extract filename and content type
+      const pdfFilename = `${req.data.FileName}.pdf`;
+      const contentType = "application/pdf";
+
+      // Read the PDF content as an ArrayBuffer
+      const pdfBuffer = await response.arrayBuffer();
+
+      if (pdfBuffer) {
+        // Convert the ArrayBuffer to a base64 string
+        const base64Data = Buffer.from(pdfBuffer).toString("base64");
+
+        // Return the response with filename, mimetype, and base64 data
+        return {
+          filename: pdfFilename,
+          mimetype: contentType,
+          data: base64Data,
         };
-
-        const response = await fetch(url, requestOptions);
-
-        if (!response.ok) {
-            throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
-        }
-
-        // Extract filename and content type
-        const pdfFilename = `${req.data.FileName}.pdf`;
-        const contentType = 'application/pdf';
-
-        // Read the PDF content as an ArrayBuffer
-        const pdfBuffer = await response.arrayBuffer();
-
-        if (pdfBuffer) {
-            // Convert the ArrayBuffer to a base64 string
-            const base64Data = Buffer.from(pdfBuffer).toString('base64');
-
-            // Return the response with filename, mimetype, and base64 data
-            return {
-                filename: pdfFilename,
-                mimetype: contentType,
-                data: base64Data
-            };
-        } else {
-            throw new Error('Empty PDF file content');
-        }
+      } else {
+        throw new Error("Empty PDF file content");
+      }
     } catch (error) {
-        console.error('Error:', error);
-        throw error; // Re-throw the error for better error handling
+      console.error("Error:", error);
+      throw error; // Re-throw the error for better error handling
     }
-});
-
-    
+  });
 });
